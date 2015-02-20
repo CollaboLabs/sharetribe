8b4d42bd15d92a50254af3f15d3251a91a7633d9
Mikko Koski <mikkokos@gmail.com>
2015-02-20 15:02:10 +0200

    Read from ListingShape
    - New listing
    - New/edit categories

--- app/models/listing_shape.rb
+++ app/models/listing_shape.rb
@@ -24,4 +24,8 @@ class ListingShape < ActiveRecord::Base
 
   has_one :transaction_process
   has_many :translations, :class_name => "ListingShapeTranslation", :dependent => :destroy
+
+  def display_name(locale)
+    TranslationCache.new(self, :translations).translate(locale, :name)
+  end
 end
